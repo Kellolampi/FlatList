cdd29ed8fd108aa0749e7ef451f483a0e4aa40a3
Kimi Kellolampi <t2keki00@students.oamk.fi>
2024-02-06 04:18:53 +0200

    eka

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {DATA} from './Data'
+import Row from './components/Row'
+import Search from './components/Search'
+import React, { useState, useEffect } from 'react'
+
+export default function App() {
+  const[items, setItems] = useState([])
+
+  useEffect(() => {
+    setItems(DATA)
+  },[])
+
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search))
+    setItems(searchArray)
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch}/>
+      <FlatList
+        data={items}
+        renderItem = {({item}) => (
+          <Row person={item} />
+        )}
+        ></FlatList>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
